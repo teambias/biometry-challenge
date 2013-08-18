87d131cd41309a453e25ac0143097bbbe0ad308f
thomasburgess <thomas.t.burgess@gmail.com>
2013-08-18 15:43:09 +0200

    Clean up

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ biometry-challenge
 ==================
 
 Biometry challenge
+
+* biometric.py - makes db from csv files, adds summary tables and normalises them
--- biometric.py
+++ biometric.py
@@ -3,6 +3,11 @@ import csv
 import numpy as np
 
 
+summary_columns = (
+    ("range", "REAL"), ("min", "REAL"), ("max", "REAL"),
+    ("avg", "REAL"), ("variance", "REAL"), ("Device", "INTEGER"))
+
+
 def create_table(dbfile, columns, table):
     """
     Create table with given columns
@@ -116,94 +121,71 @@ def create_table_from_csv(dbfile, datadir="data/"):
     add_index(dbfile, "train", "Device")
 
 
-def create_training_summary_table(dbfile):
+def create_summary_table(dbfile, table, key):
+    """
+    Creates a summary table with a certain key
+
+    Parameters:
+    * dbfile - database to write to
+    * table - table to loop over
+    * key - column in table to group by
+    """
+    create_table(dbfile, summary_columns, table)
+    with sql.connect(dbfile) as con:
+        cur = con.cursor()
+        cur.execute("""INSERT INTO
+            train_summary(range,min,max,avg,variance,{0}) SELECT
+            MAX(d)-MIN(d) AS range,
+            MIN(d) AS min,
+            MAX(d) AS max,
+            AVG(d) as avg,
+            AVG(d*d)-AVG(d)*AVG(d) AS variance,
+            {0}
+            FROM (
+                SELECT t.X*t.X+t.Y*t.Y+t.Z*t.Z AS d, t.0 as {0}
+                FROM {1} t) GROUP BY {0};""".format(key, table))
+
+
+def create_summarys_tables(dbfile):
     """
     Create training summary table in dbfile
 
     Parameters:
     * dbfile - database to write to
     """
-    columns = (("range", "REAL"), ("min", "REAL"), ("max", "REAL"),
-               ("avg", "REAL"), ("variance", "REAL"), ("Device", "INTEGER"))
-
-    create_table(dbfile, columns, "train_summary")
-
-    with sql.connect(dbfile) as con:
-        cur = con.cursor()
-        cur.execute("""INSERT INTO
-            train_summary(range,min,max,avg,variance,Device) SELECT
-            MAX(d)-MIN(d) AS range,
-            MIN(d) AS min,
-            MAX(d) AS max,
-            AVG(d) as avg,
-            AVG(d*d)-AVG(d)*AVG(d) AS variance,
-            Device
-            FROM (
-                SELECT t.X*t.X+t.Y*t.Y+t.Z*t.Z AS d, t.Device as Device
-                FROM train t) GROUP BY Device;""")
+    create_summary_table(dbfile, "train_summary", "Device")
+    create_summary_table(dbfile, "test_summary", "SequenceId")
 
 
-def create_testing_summary_table(dbfile):
+def make_transformed_table(dbfile):
     """
-    Create testing summary table in dbfile
+    Make the transformed table
 
     Parameters:
     * dbfile - database to write to
     """
-    columns = (("range", "REAL"), ("min", "REAL"), ("max", "REAL"),
-               ("avg", "REAL"), ("variance", "REAL"),
-               ("SequenceId", "INTEGER"))
-
-    create_table(dbfile, columns, "test_summary")
-
+    keys = [c[0] for c in summary_columns][:-1]
+    create_table(dbfile, summary_columns, "train_summary_transformed")
     with sql.connect(dbfile) as con:
         cur = con.cursor()
-        cur.execute("""INSERT INTO
-            test_summary(range,min,max,avg,variance,SequenceId) SELECT
-            MAX(d)-MIN(d) AS range,
-            MIN(d) AS min,
-            MAX(d) AS max,
-            AVG(d) as avg,
-            AVG(d*d)-AVG(d)*AVG(d) AS variance,
-            SequenceId
-            FROM (
-                SELECT t.X*t.X+t.Y*t.Y+t.Z*t.Z AS d, t.SequenceId as SequenceId
-                FROM test t) GROUP BY SequenceId;""")
 
-
-def make_transformed_columns(dbfile, keys):
-    """
-    Make SQL command fragment to transform sql variables so that they are
-    normalized to training data.
-    """
-    with sql.connect(dbfile) as con:
-        cur = con.cursor()
+        # Make the transform command
         cur.execute("SELECT {} FROM train_summary".format(
                     ",".join([k for k in keys])))
         dat = np.array(cur.fetchall())
         mmin = np.min(dat, axis=0)
         mmax = np.max(dat, axis=0)
-        cmd = ", \n".join(
+        transform_command = ", \n".join(
             ["({0}-{1})/{2} AS t_{0}".format(
                 x[0], x[1], x[2]) for x in zip(keys, mmin, mmax - mmin)[:-1]])
-        return cmd
 
-
-def make_transformed_table(dbfile):
-    """
-    """
-    columns = (("range", "REAL"), ("min", "REAL"), ("max", "REAL"),
-               ("avg", "REAL"), ("variance", "REAL"), ("Device", "INTEGER"))
-    keys = [c[0] for c in columns][:-1]
-    trf = make_transformed_columns(dbfile, keys)
-    # create_table(dbfile, columns, "train_summary_transformed")
-    with sql.connect(dbfile) as con:
-        cur = con.cursor()
         # Get transformed training data
-        cur.execute("SELECT {}, Device FROM train_summary".format(trf))
+        cur.execute(
+            "SELECT {}, Device FROM train_summary".format(transform_command))
         print cur.fetchall()
         # Get transformed testing data
-        cur.execute("SELECT {}, SequenceId FROM test_summary".format(trf))
+        cur.execute(
+            "SELECT {}, SequenceId FROM test_summary".format(transform_command))
         print cur.fetchall()
 
 datadir = "data/"
