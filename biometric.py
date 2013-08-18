99ca51332969bf028347d9bb73b8ef1c6a12e58e
thomasburgess <thomas.t.burgess@gmail.com>
2013-08-18 17:42:00 +0200

    Rewrote table making

--- biometric.py
+++ biometric.py
@@ -2,10 +2,19 @@ import sqlite3 as sql
 import csv
 import numpy as np
 
+# Setup LOGGER
+import logging as LOGGER
+LOGGER.basicConfig(
+    format="%(asctime)s %(levelname)s %(funcName)s:%(lineno)s - %(message)s",
+    level=LOGGER.DEBUG)
+
 
 summary_columns = (
-    ("range", "REAL"), ("min", "REAL"), ("max", "REAL"),
-    ("avg", "REAL"), ("variance", "REAL"), ("Device", "INTEGER"))
+    ("range", "REAL", "MAX(d)-MIN(d)"),
+    ("min", "REAL", "MIN(d)"),
+    ("max", "REAL", "MAX(d)"),
+    ("avg", "REAL", "AVG(d)"),
+    ("variance", "REAL", "AVG(d*d)-AVG(d)*AVG(d)"))
 
 
 def create_table(dbfile, columns, table):
@@ -13,11 +22,12 @@ def create_table(dbfile, columns, table):
     Create table with given columns
 
     Parameters:
-    * dbfile - database to write to
+    * dbfile - database to use
     * columns - tuple of ("name", "type") for db
     * table - name of table to create
     """
-    print("Inserting table \"{}\" into database \"{}\"".format(table, dbfile))
+    LOGGER.debug("Inserting table \"{}\" into database \"{}\""
+                 .format(table, dbfile))
     with sql.connect(dbfile) as con:
         cur = con.cursor()
 
@@ -43,12 +53,15 @@ def insert_csv_table(dbfile, csvfile, columns, table):
     * csvfile - csvfile with table to read
     * dbfile, columns, table - see create_table
     """
+    LOGGER.debug("Inserting csv \"{}\" into database \"{}\""
+                 .format(csvfile, dbfile))
+
     create_table(dbfile, columns, table)
     with sql.connect(dbfile) as con, open(csvfile, "r") as infile:
         cur = con.cursor()
 
         # Read data from csv
-        print("Reading from csv file \"{}\"".format(csvfile))
+        LOGGER.debug("Reading from csv file \"{}\"".format(csvfile))
         csvdata = csv.reader(infile)
         floatdata = None
         for row in csvdata:
@@ -58,7 +71,8 @@ def insert_csv_table(dbfile, csvfile, columns, table):
             floatdata += [[float(i) for i in row]]
 
         # Write data to new table
-        print("Write data into table {} in dbfile {}".format(table, dbfile))
+        LOGGER.debug("Write data into table {} in dbfile {}"
+                     .format(table, dbfile))
         cols = ",".join(["{}".format(c[0]) for c in columns])
         vals = ",".join(["?" for c in columns])
         cur.executemany(
@@ -73,20 +87,21 @@ def add_index(dbfile, table, key):
     Will drop prexisting index!
 
     Parameters:
-    * dbfile - database file
+    * dbfile - database to use
     * table - table to index in db
     * key - key to index in table
     """
     indexname = "idx_{}_{}".format(table, key)
-    print("Adding index \"{}\" to table \"{}\" on key \"{}\" in db \"{}\"".
-          format(indexname, table, key, dbfile))
+    LOGGER.debug(
+        "Adding index \"{}\" to table \"{}\" on key \"{}\" in db \"{}\""
+        .format(indexname, table, key, dbfile))
     with sql.connect(dbfile) as con:
         cur = con.cursor()
         cur.execute("DROP INDEX IF EXISTS {};".format(indexname))
         cur.execute("CREATE INDEX {} ON {}({});".format(indexname, table, key))
 
 
-def create_table_from_csv(dbfile, datadir="data/"):
+def create_tables_from_csv(dbfile, datadir="data/"):
     """
     Create raw table from csv.
 
@@ -96,9 +111,10 @@ def create_table_from_csv(dbfile, datadir="data/"):
     * test.csv
 
     Parameters:
-    * dbfile - database to write to
+    * dbfile - database to use
     * datadir - directory with csv files (must end with /)
     """
+    LOGGER.debug("Creating tables in database {}".format(dbfile))
 
     # Test questions
     questions_cols = (("QuestionId", "INTEGER"), (
@@ -121,77 +137,105 @@ def create_table_from_csv(dbfile, datadir="data/"):
     add_index(dbfile, "train", "Device")
 
 
-def create_summary_table(dbfile, table, key):
+def create_summary_table(dbfile, source_table, table, columns, key):
     """
     Creates a summary table with a certain key
 
     Parameters:
-    * dbfile - database to write to
+    * dbfile - database to use
     * table - table to loop over
     * key - column in table to group by
+    * columns - columns to loop over
     """
-    create_table(dbfile, summary_columns, table)
+    LOGGER.debug(
+        "Creating summary table \"{}\" by key \"{}\" in database \"{}\""
+        .format(table, key, dbfile))
+    col_keys = ", ".join(["{}".format(c[0]) for c in columns])
+    col_defs = ", ".join(["{} AS {}".format(c[2], c[0]) for c in columns])
+    cols = [c for c in columns] + [(key, 'INTEGER')]
+    create_table(dbfile, cols, table)
     with sql.connect(dbfile) as con:
         cur = con.cursor()
-        cur.execute("""INSERT INTO
-            train_summary(range,min,max,avg,variance,{0}) SELECT
-            MAX(d)-MIN(d) AS range,
-            MIN(d) AS min,
-            MAX(d) AS max,
-            AVG(d) as avg,
-            AVG(d*d)-AVG(d)*AVG(d) AS variance,
-            {0}
-            FROM (
-                SELECT t.X*t.X+t.Y*t.Y+t.Z*t.Z AS d, t.0 as {0}
-                FROM {1} t) GROUP BY {0};""".format(key, table))
+        cmd = """INSERT INTO {4}({0}, {1}) SELECT {2}, {1} FROM
+            (SELECT t.X*t.X+t.Y*t.Y+t.Z*t.Z AS d, t.{1} as {1} FROM {3} t)
+            GROUP BY {1};""".format(
+            col_keys, key, col_defs, source_table, table)
+        cur.execute(cmd)
 
 
-def create_summarys_tables(dbfile):
+def create_summary_tables(dbfile):
     """
-    Create training summary table in dbfile
+    Create training summary tables in dbfile
 
     Parameters:
-    * dbfile - database to write to
+    * dbfile - database to use
     """
-    create_summary_table(dbfile, "train_summary", "Device")
-    create_summary_table(dbfile, "test_summary", "SequenceId")
+    LOGGER.debug(
+        "Creating training summary tables in dbfile {}".format(dbfile))
+    create_summary_table(dbfile, "train", "train_summary",
+                         summary_columns, "Device")
+    create_summary_table(dbfile, "test", "test_summary",
+                         summary_columns, "SequenceId")
 
 
-def make_transformed_table(dbfile):
+def make_norm_select(dbfile, keys):
     """
-    Make the transformed table
+    Make the select command normalise columns
 
     Parameters:
-    * dbfile - database to write to
+    * dbfile - database to use
+    * keys - keys to select
     """
-    keys = [c[0] for c in summary_columns][:-1]
-    create_table(dbfile, summary_columns, "train_summary_transformed")
+    LOGGER.debug("Making the select command normalise columns for dbfile {}"
+                 .format(dbfile))
     with sql.connect(dbfile) as con:
         cur = con.cursor()
-
-        # Make the transform command
         cur.execute("SELECT {} FROM train_summary".format(
                     ",".join([k for k in keys])))
         dat = np.array(cur.fetchall())
         mmin = np.min(dat, axis=0)
         mmax = np.max(dat, axis=0)
-        transform_command = ", \n".join(
+        return ", \n".join(
             ["({0}-{1})/{2} AS t_{0}".format(
                 x[0], x[1], x[2]) for x in zip(keys, mmin, mmax - mmin)[:-1]])
 
-        # Get transformed training data
-        cur.execute(
-            "SELECT {}, Device FROM train_summary".format(transform_command))
-        print cur.fetchall()
-        # Get transformed testing data
-        cur.execute(
-            "SELECT {}, SequenceId FROM test_summary".format(transform_command))
-        print cur.fetchall()
+
+def create_norm_table(dbfile, source_table, table, columns, key):
+    """
+    Create a normalised table
+    Parameters:
+    * dbfile - database to use
+    * source_table - table to normalise*
+    * table - destination table
+    * columns - columns to normalise
+    """
+    LOGGER.debug(
+        "Creating normalised table \"{}\" from " + "source table \"{}\" in database \"{}\"".format(table, source_table, dbfile))
+
+
+def make_normed_summaries(dbfile):
+    """
+    Make the transformed tables
+
+    Parameters:
+    * dbfile - database to use
+    """
+    LOGGER.debug("Making the transformed tables in in dbfile \"{}\""
+                 .format(dbfile))
+    keys = [c[0] for c in summary_columns][:-1]
+    norm = make_norm_select(dbfile, keys)
+    norm_columns = [(i[0][0], i[0][1], i[1])
+                    for i in zip(summary_columns, norm)]
+    create_summary_table(dbfile, "train_summary_norm", "Device", norm_columns)
+    create_summary_table(
+        dbfile, "test_summary_norm", "SequenceId", norm_columns)
+
 
 datadir = "data/"
-dbfile = datadir + "biometric_data.sqlite"
-# create_table_from_csv(dbfile, datadir)  # Don't rerun if db is ok
-# create_training_summary_table(dbfile)   # Also not this one
-# create_testing_summary_table(dbfile)   # ... yes not this either...
+# dbfile = datadir + "biometric_data.sqlite"
+dbfile = datadir + "biometric_data_2.sqlite"
+# create_tables_from_csv(dbfile, datadir)  # Don't rerun if db is ok
+create_summary_tables(dbfile)
+# make_normed_summaries(dbfile)
 
-make_transformed_table(dbfile)
+LOGGER.debug("Done work with dbfile \"{}\"".format(dbfile))
